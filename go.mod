2f40308bdddd0efa7f3cd3f07601a5495c37ca93
Jonas Alberto <jonasalberto_8@hotmail.com>
2024-11-01 04:16:19 -0300

    Implementação da rota /publicacoes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mentoriadevops/planet
 
 go 1.22.5
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handlePublicacoes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+
+	response := map[string]interface{}{
+		"message":     "Publicações",
+		"publicações": []string{"publicação 1", "publicação 2"},
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
 
 func main() {
-	fmt.Println("Olá mundo!")
+
+	r := mux.NewRouter()
+	fmt.Println("Iniciando o servidor...")
+
+	r.HandleFunc("/publicacoes", handlePublicacoes).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
